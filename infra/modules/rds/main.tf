49be5d5b1baecf8e5e8886a2a503c85bbe08e67d
libelola <fabriciod08@gmail.com>
2026-02-13 20:18:13 -0300

    no God, please no

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -25,7 +25,7 @@ resource "aws_db_instance" "this" {
   instance_class    = "db.t3.micro"
   allocated_storage = 20
 
-  db_name  = var.db_name
+  
   username = var.username
   password = var.password
 
